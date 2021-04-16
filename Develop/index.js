27df5a0ad82cac0e174de045fcef832173974401
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-15 23:33:22 -0500

    linking modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,82 +1,83 @@
 // TODO: Include packages needed for this application
 const questionPrompts = require('inquirer');
 const fs = require('fs');
-const generateMarkdown=require('generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 const util = require('util');
 
 // create writeFile function using promises
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions=()=>{
+const questions = () => {
     return inquirer.prompt(
         [
             {
                 type: 'input',
                 message: 'What is the name of your project?',
                 name: 'title',
-              },
-              {
+            },
+            {
                 type: 'list',
                 message: 'What is your license type?',
-                choices: ['','',''],
+                choices: ['', '', ''],
                 name: 'licenseType',
-              },
-              {
+            },
+            {
                 type: 'input',
                 message: 'What is your license code?',
                 name: 'license',
-              },
-              {
+            },
+            {
                 type: 'input',
                 message: 'Please provide a brief description of your project.',
                 name: 'description',
-              },
-              {
+            },
+            {
                 type: 'input',
                 message: 'What is the usage of this project?',
                 name: 'usage',
-              },
-              {
+            },
+            {
                 type: 'input',
                 message: 'How would a user install this project?',
                 name: 'install',
-              },
-              {
+            },
+            {
                 type: 'input',
                 message: 'What type of tests have been run on the project?',
                 name: 'tests',
-              },
-              {
+            },
+            {
                 type: 'input',
                 message: 'How would other developers make contributions to this project?',
                 name: 'contrib',
-              },
-              {
+            },
+            {
                 type: 'input',
                 message: 'Please provide your gitHub profile link.',
                 name: 'github',
-              },
-              {
+            },
+            {
                 type: 'input',
                 message: 'Please provide your email address.',
                 name: 'email',
-              },
-            ]
+            },
+        ]
     )
 }
 
 
 // TODO: Create a function to write README file
-function generateREADME(fileName, data) {}
+function generateREADME(fileName, data) { }
 
 // TODO: Create a function to initialize app
 const init = () => {
     questions()
-      .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
-      .then(() => console.log('Successfully wrote to README'))
-      .catch((err) => console.error(err));
-  };
+        .then((answers) => module.exports = answers.licenseType)
+        .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
+        .then(() => console.log('Successfully wrote to README'))
+        .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,4 @@
+const licenseSelection=require('../index')
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
